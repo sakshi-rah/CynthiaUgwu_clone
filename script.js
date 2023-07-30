9d42ec6a3d602a1c5ee2da979ae85990da42cfeb
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2023-07-30 13:34:10 +0530

    feat: home cursor style

--- script.js
+++ script.js
@@ -33,6 +33,32 @@ function firstPageAnim() {
       ease: Expo.easeInOut,
     });
 }
+function circleChaptaKaro() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
 
 circleMouseFollower();
 firstPageAnim();
