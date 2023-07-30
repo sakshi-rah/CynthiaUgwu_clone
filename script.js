c9799650932f8408f5cd355e7851029971f521f4
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2023-07-30 11:02:31 +0530

    feat: home anim added

--- script.js
+++ script.js
@@ -6,8 +6,33 @@ const scroll = new LocomotiveScroll({
 function circleMouseFollower() {
   window.addEventListener("mousemove", function (dets) {
     document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-  })
-
-
+  });
 }
+function firstPageAnim() {
+
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
 circleMouseFollower();
+firstPageAnim();
