8b0e46606048e6facdbe8bcd3f3cdc11f3684646
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2023-07-30 14:28:44 +0530

    final touch

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function circleChaptaKaro() {
   var yprev = 0;
 
   window.addEventListener("mousemove", function (dets) {
-    clearTimeout(timeout);
+
     xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
     yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
 
@@ -19,11 +19,7 @@ function circleChaptaKaro() {
     yprev = dets.clientY;
 
     circleMouseFollower(xscale, yscale);
-    timeout = setTimeout(function () {
-      document.querySelector(
-        "#minicircle"
-      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
-    }, 100);
+
   });
 }
 circleChaptaKaro();
